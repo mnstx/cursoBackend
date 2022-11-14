60378c1bf49e3a247a002ecc14ecfdeafea8a74a
mnstx <103534346+mnstx@users.noreply.github.com>
2022-11-14 15:34:16 -0300

    Delete proyectopoketienda/proyectopoketienda directory

--- proyectopoketienda/proyectopoketienda/delete/deletepokemons.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $conexion = mysqli_connect("127.0.0.1", "root", "");
-  mysqli_select_db($conexion, "pokestore");
-  $id = $_GET['id'];
-  $consulta = "DELETE FROM `pokemon` WHERE `id`=$id";
-  mysqli_query($conexion,$consulta);
-  header('location: ../admin.php');
-?>
